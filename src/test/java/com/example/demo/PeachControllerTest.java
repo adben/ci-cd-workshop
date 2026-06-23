4c628dad14a4123dc665b5742a6a542cd58c5e05
Adolfo Benedetti <adolfo.benedettivelasquez@rws.nl>
2026-06-23 16:45:55 +0200

    level 3b

--- src/test/java/com/example/demo/PeachControllerTest.java
+++ src/test/java/com/example/demo/PeachControllerTest.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -17,8 +18,9 @@ class PeachControllerTest {
 
     @Test
     void getPeach_shouldReturnCatchPhrase() throws Exception {
-        //TODO: Implement test
-    }
+       mockMvc.perform(get("/peach"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("I'm Princess Peach!"));    }
 
     @Test
     void postPeach_shouldReturnWeirdPostPhrase() throws Exception {
